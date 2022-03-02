1445ead5e7cdd2ca740119f6cde12b4f70670d4c
Julia-Fremder <juliafremder@gmail.com>
2022-03-02 11:03:37 +0000

    duration function

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,26 @@ function App() {
     setAppointmentAudios(result.data as audioDB[])
   }
 
+  const handleDuration = () => {
+    // Create a non-dom allocated Audio element
+var au = document.createElement('audio');
+
+// Define the URL of the MP3 audio file
+au.src = "https://mydomain.com/myaudio.mp3";
+
+// Once the metadata has been loaded, display the duration in the console
+au.addEventListener('loadedmetadata', function(){
+    // Obtain the duration in seconds of the audio file (with milliseconds as well, a float value)
+    var duration = au.duration;
+
+    // example 12.3234 seconds
+    console.log("The duration of the song is of: " + duration + " seconds");
+    // Alternatively, just display the integer value with
+    // parseInt(duration)
+    // 12 seconds
+},false);
+  }
+
   useEffect(() =>{
    getAudioByAppointment(idAppt).then(result => setAppointmentAudios(result.data as audioDB[]))
   }, [])
